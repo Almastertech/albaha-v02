0cdfdc2352add910d32dc03275ffe1e8c2c5b1c2
Almastertech <youssefm.f45@gmail.com>
2025-04-23 18:58:17 +0200

    Refactored the component

--- components/homepage/AlbahaStats.jsx
+++ components/homepage/AlbahaStats.jsx
@@ -22,13 +22,13 @@ export default function AlbahaStats({ data }) {
   const videoRef = useRef(null);
 
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 });
-  const files_path = process.env.NEXT_PUBLIC_FILES_PATH;
+
   if (inView && videoRef.current) {
     videoRef.current.play().catch((e) => console.warn("Autoplay failed", e));
   }
 
   const handleTimeUpdate = () => {
-    if (videoRef.current.currentTime >= 13 && !fadeOut) {
+    if (videoRef.current.currentTime >= 0 && !fadeOut) {
       setFadeOut(true);
       setShowContent(true);
     }
@@ -53,58 +53,11 @@ export default function AlbahaStats({ data }) {
           fadeOut={fadeOut}
         />
         {showContent && (
-          <AnimatePresence>
-            <motion.div className="absolute inset-0 flex justify-center items-center bg-[#333]">
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: currentArea ? 0 : 1,
-                  transition: { duration: 0.5, delay: 0.2 },
-                }}
-                exit={{ opacity: 1, transition: { duration: 0.5 } }}
-                className="absolute inset-0">
-                <Image
-                  src={`/assets/home/stats/albaha_states/background.png`}
-                  alt="background"
-                  fill
-                  priority
-                  className="object-cover object-center"
-                  quality={100}
-                  unoptimized
-                />
-              </motion.div>
-
-              {data.map((state) => (
-                <motion.div
-                  key={state.name.arabic}
-                  initial={{ opacity: 0 }}
-                  animate={{
-                    opacity:
-                      state.state === currentArea || state.state === clickedArea
-                        ? 1
-                        : 0,
-                    transition: { duration: 0.5 },
-                  }}
-                  exit={{
-                    opacity: 0,
-                    transition: { duration: 0.5, delay: 0.2 },
-                  }}
-                  className="absolute inset-0">
-                  <Image
-                    src={`${files_path}${state.url}`}
-                    alt={state.name.arabic || "state name"}
-                    fill
-                    priority
-                    className="object-cover object-center"
-                    quality={100}
-                    unoptimized
-                  />
-                </motion.div>
-              ))}
-
-              <CloudsLayer />
-            </motion.div>
-          </AnimatePresence>
+          <StatsImages
+            currentArea={currentArea}
+            data={data}
+            clickedArea={clickedArea}
+          />
         )}
         <ContentOverlay
           setClickedArea={setClickedArea}
@@ -118,6 +71,65 @@ export default function AlbahaStats({ data }) {
     </section>
   );
 }
+
+const StatsImages = ({ currentArea, data, clickedArea }) => {
+  const files_path = process.env.NEXT_PUBLIC_FILES_PATH;
+
+  return (
+    <AnimatePresence>
+      <motion.div className="absolute inset-0 flex justify-center items-center bg-[#333]">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: currentArea ? 0 : 1,
+            transition: { duration: 0.5, delay: 0.2 },
+          }}
+          exit={{ opacity: 1, transition: { duration: 0.5 } }}
+          className="absolute inset-0">
+          <Image
+            src={`/assets/home/stats/albaha_states/background.png`}
+            alt="background"
+            fill
+            priority
+            className="object-cover object-center"
+            quality={100}
+            unoptimized
+          />
+        </motion.div>
+
+        {data.map((state) => (
+          <motion.div
+            key={state.name.arabic}
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity:
+                state.state === currentArea || state.state === clickedArea
+                  ? 1
+                  : 0,
+              transition: { duration: 0.5 },
+            }}
+            exit={{
+              opacity: 0,
+              transition: { duration: 0.5, delay: 0.2 },
+            }}
+            className="absolute inset-0">
+            <Image
+              src={`${files_path}${state.url}`}
+              alt={state.name.arabic || "state name"}
+              fill
+              priority
+              className="object-cover object-center"
+              quality={100}
+              unoptimized
+            />
+          </motion.div>
+        ))}
+
+        <CloudsLayer />
+      </motion.div>
+    </AnimatePresence>
+  );
+};
 
 const ContentOverlay = ({
   setCurrentArea,
@@ -151,7 +163,7 @@ const ContentOverlay = ({
   return (
     <div
       ref={containerRef}
-      className="w-full h-full  absolute text-white /20 pt-18 p-10 flex flex-col z-[9]">
+      className="w-full h-full   absolute text-white /20 pt-18 p-10 flex flex-col z-[9]">
       <div className="w-full h-full flex flex-col gap-8 px-10 relative">
         <LineTitle className={`mx-30  mt-10 `}>بيانات منطقة الباحة</LineTitle>
         <Mapping
@@ -161,60 +173,59 @@ const ContentOverlay = ({
           showContent={showContent}
           setCurrentArea={setCurrentArea}
         />
-        {currentAreaInfo ? (
-          <motion.div
-            className="flex items-center justify-between w-full h-fit mt-20"
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
-            viewport={{ once: false }}>
-            <div className="w-full flex flex-col items-start gap-6 justify-center">
-              {currentAreaInfo.sectorIndicators
-                .slice(
-                  0,
-                  Math.ceil(currentAreaInfo.sectorIndicators.length / 2)
-                )
-                .map((item, index) => (
-                  <StateInfo key={index} index={index} data={item} />
-                ))}
-            </div>
-            <div className="w-[60%] mr-120 flex flex-col items-start gap-6 justify-start">
-              {currentAreaInfo.sectorIndicators
-                .slice(Math.ceil(currentAreaInfo.sectorIndicators.length / 2))
-                .map((item, index) => (
-                  <StateInfo index={index} key={index} data={item} />
-                ))}
-            </div>
-          </motion.div>
-        ) : (
-          showContent && (
-            <motion.div
-              className="flex items-center justify-between w-full h-fit mt-20"
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6, ease: "easeOut" }}
-              viewport={{ once: true }}>
-              <div className="w-full flex flex-col items-start gap-6 justify-center">
-                {default_states_data
-                  .slice(0, Math.ceil(default_states_data.length / 2))
-                  .map((item, index) => (
-                    <StateInfo index={index} key={index} data={item} />
-                  ))}
-              </div>
-              <div className="w-[60%] mr-120 flex flex-col items-start gap-6 justify-start">
-                {default_states_data
-                  .slice(Math.ceil(default_states_data.length / 2))
-                  .map((item, index) => (
-                    <StateInfo index={index} key={index} data={item} />
-                  ))}
-              </div>
-            </motion.div>
-          )
-        )}
+        <Content currentAreaInfo={currentAreaInfo} showContent={showContent} />
         <div data-ignore-click>
           {showContent && <Button>مشاهدة المزيد</Button>}
         </div>
       </div>
     </div>
+  );
+};
+
+const Content = ({ currentAreaInfo, showContent }) => {
+  return currentAreaInfo ? (
+    <motion.div
+      className="flex items-center justify-between w-full h-fit mt-20"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}>
+      <div className="w-full flex flex-col items-start gap-6 justify-center">
+        {currentAreaInfo.sectorIndicators
+          .slice(0, Math.ceil(currentAreaInfo.sectorIndicators.length / 2))
+          .map((item, index) => (
+            <StateInfo key={index} index={index} data={item} />
+          ))}
+      </div>
+      <div className="w-[60%] mr-120 flex flex-col items-start gap-6 justify-start">
+        {currentAreaInfo.sectorIndicators
+          .slice(Math.ceil(currentAreaInfo.sectorIndicators.length / 2))
+          .map((item, index) => (
+            <StateInfo index={index} key={index} data={item} />
+          ))}
+      </div>
+    </motion.div>
+  ) : (
+    showContent && (
+      <motion.div
+        className="flex items-center justify-between w-full h-fit mt-20"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}>
+        <div className="w-full flex flex-col items-start gap-6 justify-center">
+          {default_states_data
+            .slice(0, Math.ceil(default_states_data.length / 2))
+            .map((item, index) => (
+              <StateInfo index={index} key={index} data={item} />
+            ))}
+        </div>
+        <div className="w-[60%] mr-120 flex flex-col items-start gap-6 justify-start">
+          {default_states_data
+            .slice(Math.ceil(default_states_data.length / 2))
+            .map((item, index) => (
+              <StateInfo index={index} key={index} data={item} />
+            ))}
+        </div>
+      </motion.div>
+    )
   );
 };
